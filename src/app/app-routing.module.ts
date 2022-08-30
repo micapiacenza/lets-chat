d1e95a00aa04186c08473d8a515b69d494e83c7e
micapiacenza <micapiacenza219@gmail.com>
2022-08-30 20:42:43 +1000

    Updating main chat expandable card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import {LoginComponent} from "./views/login/login.component";
 import {CreateAccountComponent} from "./views/create-account/create-account.component";
 import {ProfileComponent} from "./views/profile/profile.component";
 import {MainChatComponent} from "./views/main-chat/main-chat.component";
+import {CreateRoomComponent} from "./views/profile/views/create-room/create-room.component";
+import {CreateGroupComponent} from "./views/profile/views/create-group/create-group.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -12,6 +14,8 @@ const routes: Routes = [
   {path: 'create-account', component: CreateAccountComponent},
   {path: 'profile', component: ProfileComponent},
   {path: 'main-chat', component: MainChatComponent},
+  {path: 'create-group', component: CreateGroupComponent},
+  {path: 'create-room', component: CreateRoomComponent},
 ];
 
 @NgModule({
--- src/app/views/main-chat/common/components/expandable-group-card/expandable-group-card.component.ts
+++ src/app/views/main-chat/common/components/expandable-group-card/expandable-group-card.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./expandable-group-card.component.css']
 })
 export class ExpandableGroupCardComponent implements OnInit {
-
+  public groupList = [
+    'Group 1',
+    'Group 2',
+    'Group 3',
+    'Group 4',
+    'Group 5',
+  ];
+  public roomList = [
+    'Room 1',
+    'Room 2',
+    'Room 3',
+  ];
+  public indexExpanded: number= -1;
+  public isExpand: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public expandCard() {
+    this.isExpand = ! this.isExpand;
+  }
 }
